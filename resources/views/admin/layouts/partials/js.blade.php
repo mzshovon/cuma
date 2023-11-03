18e52ed333034a7f94c4538ca2d3e4533c4f4c0b
Mohammad Moniruzzaman <mohammad.moniruzzaman@brainstation-23.com>
2023-11-04 01:16:29 +0600

    assets refactored

--- resources/views/admin/layouts/partials/js.blade.php
+++ resources/views/admin/layouts/partials/js.blade.php
@@ -1,18 +1,18 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="/cuma/public/assets/vendor/jquery/jquery.min.js"></script>
+<script src="{{URL::to('/')}}/public/assets/vendor/jquery/jquery.min.js"></script>
 {{--  <script src="/cuma/public/') }}admin/assets/vendor/apexcharts/apexcharts.min.js"></script>  --}}
-<script src="/cuma/public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{URL::to('/')}}/public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 {{--  <script src="/cuma/public/') }}admin/assets/vendor/jqueryui/js/jquery-ui.min.js"></script>  --}}
 {{--  <script src="/cuma/public/') }}admin/assets/vendor/chart.js/chart.umd.js"></script>  --}}
 {{--  <script src="/cuma/public/') }}admin/assets/vendor/echarts/echarts.min.js"></script>  --}}
 {{--  <script src="/cuma/public/') }}admin/assets/vendor/quill/quill.min.js"></script>  --}}
-<script src="/cuma/public/assets/vendor/simple-datatables/simple-datatables.js"></script>
-<script src="/cuma/public/assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="{{URL::to('/')}}/public/assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="{{URL::to('/')}}/public/assets/vendor/tinymce/tinymce.min.js"></script>
 {{--  <script src="/cuma/public/') }}admin/assets/vendor/php-email-form/validate.js"></script>  --}}
 
-<script src="/cuma/public/assets/js/main.js"></script>
+<script src="{{URL::to('/')}}/public/assets/js/main.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/js/toastr.js"></script>
 
 @stack('script')
--- resources/views/frontend/partials/app.blade.php
+++ resources/views/frontend/partials/app.blade.php
@@ -8,26 +8,26 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="/cuma/public/frontend/images/CUMA-Logo.png" rel="icon">
-    <link href="/cuma/public/frontend/images/CUMA-Logo.png" rel="apple-touch-icon">
+    <link href="{{URL::to('/')}}/public/frontend/images/CUMA-Logo.png" rel="icon">
+    <link href="{{URL::to('/')}}/public/frontend/images/CUMA-Logo.png" rel="apple-touch-icon">
 
     <!-- Font Icon -->
-    <link href="/cuma/public/frontend/fonts/material-icon/css/material-design-iconic-font.min.css" rel="stylesheet">
-    <link href="/cuma/public/frontend/vendor/nouislider/nouislider.min.css" rel="stylesheet">
+    <link href="{{URL::to('/')}}/public/frontend/fonts/material-icon/css/material-design-iconic-font.min.css" rel="stylesheet">
+    <link href="{{URL::to('/')}}/public/frontend/vendor/nouislider/nouislider.min.css" rel="stylesheet">
 
     <!-- Main css -->
-    <link href="/cuma/public/frontend/css/style.css" rel="stylesheet">
+    <link href="{{URL::to('/')}}/public/frontend/css/style.css" rel="stylesheet">
 </head>
 <body>
 
 @yield('content')
 
-<script src="/cuma/public/frontend/vendor/jquery/jquery.min.js"></script>
-<script src="/cuma/public/frontend/vendor/nouislider/nouislider.min.js"></script>
-<script src="/cuma/public/frontend/vendor/wnumb/wNumb.js"></script>
-<script src="/cuma/public/frontend/vendor/jquery-validation/dist/jquery.validate.min.js"></script>
-<script src="/cuma/public/frontend/vendor/jquery-validation/dist/additional-methods.min.js"></script>
-<script src="/cuma/public/frontend/js/main.js"></script>
+<script src="{{URL::to('/')}}/public/frontend/vendor/jquery/jquery.min.js"></script>
+<script src="{{URL::to('/')}}/public/frontend/vendor/nouislider/nouislider.min.js"></script>
+<script src="{{URL::to('/')}}/public/frontend/vendor/wnumb/wNumb.js"></script>
+<script src="{{URL::to('/')}}/public/frontend/vendor/jquery-validation/dist/jquery.validate.min.js"></script>
+<script src="{{URL::to('/')}}/public/frontend/vendor/jquery-validation/dist/additional-methods.min.js"></script>
+<script src="{{URL::to('/')}}/public/frontend/js/main.js"></script>
 
 </body>
 </html>
